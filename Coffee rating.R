71463a83fea3559a44bc9837190d7e4a08d7eda9
Amal-Almutairy <71967687+Amal-Almutairy@users.noreply.github.com>
2020-10-15 15:43:53 +0300

    Add Chickwts Files

---
+++ Coffee rating.R
@@ -0,0 +1,37 @@
+# Coffee ratings analysis
+# 10/11/2020
+# Amal Almutairy
+library(tidyverse)
+library(rio)
+
+
+
+# coffee quality database:
+View(merged_data_cleaned)
+library(readr)
+merged_data_cleaned <- read_csv("Coffee/Coffee Quality database/merged_data_cleaned.csv")
+Arabica_data_cleaned <- read_csv("Coffee/Coffee Quality database/merged_data_cleaned.csv")
+Robusta_data_cleaned <- read_csv("Coffee/Coffee Quality database/merged_data_cleaned.csv")
+
+
+
+
+# Get familiar with our data
+summary(merged_data_cleaned)
+names(merged_data_cleaned)
+glimpse(merged_data_cleaned)
+
+# more detail:
+attributes(merged_data_cleaned)
+typeof(merged_data_cleaned)
+
+# Examine the data:
+str(Arabica_data_cleaned)
+summary(Arabica_data_cleaned)
+
+
+
+
+# arrange by region
+
+
---
+++ chickwts.R
@@ -0,0 +1,50 @@
+library(tidyverse)
+
+
+# Built in data sets:
+chickwts %>% 
+  group_by(feed) %>%
+ summarise(n = n(),
+               average = mean(weight),
+               SD = sd(weight))
+
+
+
+
+# Plot 1
+ggplot(chickwts, aes(feed, weight)) +
+  geom_boxplot()
+
+
+
+# Plot 2
+ggplot(chickwts, aes(feed, weight)) +
+  geom_jitter(width = 0.15) +
+  stat_summary(fun.data = mean_sdl, 
+               fun.args = list(mult = 1), 
+               col = "red")
+
+
+
+# Inferential Statistics
+chickwts_lm <- lm(weight ~ feed , data =  chickwts)
+chickwts_lm
+anova(chickwts_lm)
+
+
+# Rate of change
+chickwts_lm <- chickwts_lm %>% 
+chickwts_lm <- (diff [weight ~ feed] - value[data =  chickwts]) %>% 
+  arrange(-diff)
+ 
+
+chickwts.av <- aov(weight ~ feed, data = chickwts)
+tukeyTest <- TukeyHSD(chickwts.ov)
+
+tukeyTest
+typeof(tukeyTest)
+calss(tukeyTest)
+names(tukeyTest)
+
+data.table(tukeyTest) 
+
