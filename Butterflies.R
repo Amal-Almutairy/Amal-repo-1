cd8fe8c8493474a44d234da43c76938766d355bc
Amal-Almutairy <71967687+Amal-Almutairy@users.noreply.github.com>
2020-10-15 22:28:20 +0300

    Add: Butterflies files

---
+++ Butterflies.R
@@ -0,0 +1,46 @@
+# Butterflies analysis
+# Amal Almutairy
+# 14.10.2020
+# Small case study to analyse 4 diffrent kinds of Butterflies in a garden
+library(tidyverse)
+
+
+# In a Garden I saw 4 spices of butterflies, which were:
+# 1- Milbert:
+Milbert <- 300
+
+# the wingspan:
+Milbert_wingspan <- 4.2
+
+
+# 2- Zebra:
+Zebra <- 45
+
+# the wingspan:
+Zebra_wingspan <- 5
+
+# 3- Monarch
+Monarch <- 200
+
+# the wingspan:
+Monarch_wingspan <- 8.6 
+
+# 4- Julia:
+Julia <- 80
+
+# the wingspan:
+Julia_wingspan <- 4
+
+
+c("Milbert", "Zebra", "Monarch", "Julia")
+Butterflies <- c(Milbert, Zebra, Monarch, Julia)
+Numbers <- c(300, 45, 200, 80)
+wingspan <- c(4.2, 5, 8.6, 4)
+
+
+
+Butterflies <- c(Milbert = 300, Zebra = 45, Monarch = 200, Julia = 80)
+
+
+Butterflies
+wingspan
